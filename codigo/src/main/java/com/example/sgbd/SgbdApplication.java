4c16fc12c69c0c968d31a7a5b479da22f3134b6a
MaxCosta0 <maxleysoares006@gmail.com>
2022-01-17 17:28:06 -0300

    Adicionando template do spring

---
+++ codigo/src/main/java/com/example/sgbd/SgbdApplication.java
@@ -0,0 +1,13 @@
+package com.example.sgbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgbdApplication.class, args);
+	}
+
+}
